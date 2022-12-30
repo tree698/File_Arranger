e3762a411ef0496031b7f1811742c55c8ed31850
Chanwoo <tree698@gmail.com>
2022-12-30 21:30:53 +0900

    add app.js

---
+++ app.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+const folder = process.argv[2];
+const workingDir = path.join(os.homedir(), folder);
+if (!folder || !fs.existsSync(workingDir)) {
+  console.error('Please enter folder name in Pictures');
+  return;
+}
+
+const videoDir = path.join(workingDir, 'video');
+const capturedDir = path.join(workingDir, 'captured');
+const duplicatedDir = path.join(workingDir, 'duplicated');
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
+
+fs.promises
+  .readdir(workingDir) //
+  .then(processFiles)
+  .catch(console.log);
+
+function processFiles(files) {
+  files.forEach((file) => {
+    if (isVideoFile(file)) {
+      move(file, videoDir);
+    } else if (isCapturedFile(file)) {
+      move(file, capturedDir);
+    } else if (isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir);
+    }
+  });
+}
+
+function isVideoFile(file) {
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+
+function isCapturedFile(file) {
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+
+function isDuplicatedFile(files, file) {
+  if (!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+    return false;
+  }
+
+  const edited = `IMG_E${file.split('_')[1]}`;
+  const found = files.find((f) => f.includes(edited));
+  return !!found;
+}
+
+function move(file, targetDir) {
+  console.info(`move ${file} to ${path.basename(targetDir)}`);
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+  fs.promises
+    .rename(oldPath, newPath) //
+    .catch(console.error);
+}
